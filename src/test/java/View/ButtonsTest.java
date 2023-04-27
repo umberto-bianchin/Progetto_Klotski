3d8472c56d811594f1ecf8d3b81d13a042f81269
Umberto <umbertobianchin.mail@gmail.com>
2023-04-27 12:42:31 +0200

    example test-method

--- src/test/java/View/ButtonsTest.java
+++ src/test/java/View/ButtonsTest.java
@@ -7,4 +7,8 @@ import static org.junit.jupiter.api.Assertions.*;
 class ButtonsTest {
 
     @Test
+    void nomeTest(){
+        //inizializzo un oggetto
+        //uso metodo assertEquals(expected, metodoDaVerificare)
+    }
 }
