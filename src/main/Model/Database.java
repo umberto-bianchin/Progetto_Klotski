56a069234a3423c4c6ea910c9fabefd31b20de43
ricky0219 <37349867+ricky0219@users.noreply.github.com>
2023-05-03 12:12:08 +0200

    update 1

--- src/main/Model/Database.java
+++ src/main/Model/Database.java
@@ -126,9 +126,12 @@ public class Database {
         return temp;
     }
 
-    public boolean deleteGame(int id_game) {
-
-        return false;
+    public boolean deleteGame(int id_game) throws SQLException{
+        Statement stmt = conn.createStatement();
+        String query = "DELETE FROM games WHERE ID_GAME =" +id_game+";";
+        stmt.executeQuery(query);
+        stmt.close();
+        return true;
     }
 
     public int login(String username, int password) throws SQLException{
